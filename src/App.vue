29c1e45bcd985dbae8dfe8a1480f2d99aeee0251
Tunç GÜLTÜRK <tgulturk@gmail.com>
2021-09-12 21:11:53 +0300

    Changes

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <button class="button is-primary">Test</button>
-  <p class="subtitle">My first website with <strong>Bulma</strong>!</p>
-
-  <div class="dropdown">
-    <div class="dropdown-trigger">
-      <button
-        class="button"
-        aria-haspopup="true"
-        aria-controls="dropdown-menu3"
-      >
-        <span>Click me</span>
-        <span class="icon is-small">
-          <i class="fas fa-angle-down" aria-hidden="true"></i>
-        </span>
-      </button>
-    </div>
-    <div class="dropdown-menu" id="dropdown-menu3" role="menu">
-      <div class="dropdown-content">
-        <a href="#" class="dropdown-item">
-          Overview
-        </a>
-        <a href="#" class="dropdown-item">
-          Modifiers
-        </a>
-        <a href="#" class="dropdown-item">
-          Grid
-        </a>
-        <a href="#" class="dropdown-item">
-          Form
-        </a>
-        <a href="#" class="dropdown-item">
-          Elements
-        </a>
-        <a href="#" class="dropdown-item">
-          Components
-        </a>
-        <a href="#" class="dropdown-item">
-          Layout
-        </a>
-        <hr class="dropdown-divider" />
-        <a href="#" class="dropdown-item">
-          More
-        </a>
+  <section class="section">
+    <div class="container">
+      <div class="columns is-multiline is-mobile">
+        <Card
+          src="https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_SX300.jpg"
+          alt="Image"
+        />
+        <Card
+          src="https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_SX300.jpg"
+          alt="Image"
+        />
+        <Card
+          src="https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_SX300.jpg"
+          alt="Image"
+        />
+        <Card
+          src="https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_SX300.jpg"
+          alt="Image"
+        />
+        <Card
+          src="https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_SX300.jpg"
+          alt="Image"
+        />
+        <Card
+          src="https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_SX300.jpg"
+          alt="Image"
+        />
+        <Card
+          src="https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_SX300.jpg"
+          alt="Image"
+        />
+        <Card
+          src="https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_SX300.jpg"
+          alt="Image"
+        />
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
+import Card from "./components/Card.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Card,
+  },
 };
 </script>
 
@@ -60,8 +55,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="column center is-full-mobile is-one-third-tablet is-one-quarter-desktop"
+  >
+    <div class="card">
+      <div class="card-image has-text-centered">
+        <img :src="src" :alt="alt" />
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">John Smith</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ["src", "alt"],
+};
+</script>
+
+<style scoped></style>
