a1ad8551a0d9ba7d3dafd7799c67ae5278aeacf0
Tunç GÜLTÜRK <tgulturk@gmail.com>
2021-09-19 15:18:37 +0300

    Convert settings from config.json to env file

--- src/api/omdb.js
+++ src/api/omdb.js
@@ -2,15 +2,13 @@ import axios from "axios";
 
 const apiUrl = "http://www.omdbapi.com";
 
-const config = require("../config.json");
-
 const omdbApi = {
   getMories: async (q) => {
     return new Promise((resolve, reject) => {
       axios
         .get(`${apiUrl}/?s=${q}`, {
           params: {
-            apikey: config.omdbApiKey,
+            apikey: process.env.VUE_APP_API_KEY,
           },
         })
         .then((response) => {
